d43f319fc81671d02677aed2fd335332cc6d4ed4
Harulla <laura.leroychhon@gmail.com>
2024-02-26 17:40:12 +0100

    Basic chat

--- chronos/client/src/App.js
+++ chronos/client/src/App.js
@@ -19,8 +19,8 @@ import AdminDashboard from './pages/admin/AdminDashboard';
 import TestAdmin from './pages/admin/test';
 import Unauthorized from './pages/error/Unauthorized';
 import NotFound from './pages/error/NotFound';
-import MessageApp from './pages/messages';
 
+import MessageApp from './pages/messages';
 function App() {
 
   const [listCours, setListCours] = useState([]);
@@ -58,6 +58,15 @@ function App() {
         <Route path="/psw" element={<ChangePasswordForm />} exact />
         <Route path='/*' element={<NotFound /> }/>
         <Route path='/unauthorized' element={<Unauthorized/>} exact />
+        <Route path="/call" element={<CallForm />} exact />
+        <Route path="/messages" element={<MessageApp moduleId={2} />} exact />
+
+        {/* <Route path="/" element={<ClassSquare height={300} />} exact /> */}
+
+        <Route path="/email" element={<EmailForm />} exact />
+        <Route path="/export-csv" element={<CSVExportPage />} exact />
+        {/* test branch notes lucas */}
+        <Route path="/notes" element={<Notes />} exact />
       </Routes>
   );
 }
