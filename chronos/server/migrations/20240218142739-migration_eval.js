7f15b9932ee49b60081ec8dff85a3cc4211ccf26
Liixray <koda16.77@gmail.com>
2024-02-29 22:59:43 +0100

    add migrations

---
+++ chronos/server/migrations/20240218142739-migration_eval.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('PERIODE', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      libelle: {
+        type: Sequelize.STRING
+      },
+        createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+
+    await queryInterface.addColumn(
+      'EVALUATION', // table name
+      'periodeId', // new field name
+      {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'PERIODE',
+          key: 'id',
+        },
+      },
+    );
+
+    await queryInterface.addColumn(
+      'EVALUATION', // table name
+      'noteMaximale', // new field name
+      {
+        type: Sequelize.INTEGER,
+        defaultValue : 20,
+      },
+    );
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('PERIODE', 'periodeId');
+    await queryInterface.removeColumn('EVALUATION', 'noteMaximale');
+    await queryInterface.dropTable('PERIODE');
+  }
+};
